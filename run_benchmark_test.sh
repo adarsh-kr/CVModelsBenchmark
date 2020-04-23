b479e576cc54e145d749ce248804fc6eae6772cf
Adarsh Kumar <adi28kr@gmail.com>
2020-04-23 15:56:39 -0500

    ..

---
+++ run_benchmark_test.sh
@@ -0,0 +1,41 @@
+python benchmark.py --arch resnet18 --batch_size 128
+python benchmark.py --arch resnet50 --batch_size 128
+python benchmark.py --arch resnet101 --batch_size 128
+python benchmark.py --arch resnet152 --batch_size 128
+python benchmark.py --arch resnet34 --batch_size 128
+python benchmark.py --arch vgg19 --batch_size 128
+python benchmark.py --arch vgg16 --batch_size 128
+python benchmark.py --arch vgg11 --batch_size 128
+python benchmark.py --arch googlenet --batch_size 128
+
+
+python benchmark.py --arch resnet18 --batch_size 64
+python benchmark.py --arch resnet50 --batch_size 64
+python benchmark.py --arch resnet101 --batch_size 64
+python benchmark.py --arch resnet152 --batch_size 64
+python benchmark.py --arch resnet34 --batch_size 64
+python benchmark.py --arch vgg19 --batch_size 64
+python benchmark.py --arch vgg16 --batch_size 64
+python benchmark.py --arch vgg11 --batch_size 64
+python benchmark.py --arch googlenet --batch_size 64
+
+
+python benchmark.py --arch resnet18 --batch_size 32
+python benchmark.py --arch resnet50 --batch_size 32
+python benchmark.py --arch resnet101 --batch_size 32
+python benchmark.py --arch resnet152 --batch_size 32
+python benchmark.py --arch resnet34 --batch_size 32
+python benchmark.py --arch vgg19 --batch_size 32
+python benchmark.py --arch vgg16 --batch_size 32
+python benchmark.py --arch vgg11 --batch_size 32
+python benchmark.py --arch googlenet --batch_size 32
+
+python benchmark.py --arch resnet18 --batch_size 256
+python benchmark.py --arch resnet50 --batch_size 256
+python benchmark.py --arch resnet101 --batch_size 256
+python benchmark.py --arch resnet152 --batch_size 256
+python benchmark.py --arch resnet34 --batch_size 256
+python benchmark.py --arch vgg19 --batch_size 256
+python benchmark.py --arch vgg16 --batch_size 256
+python benchmark.py --arch vgg11 --batch_size 256
+python benchmark.py --arch googlenet --batch_size 256
